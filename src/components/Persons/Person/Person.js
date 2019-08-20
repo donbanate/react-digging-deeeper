0f0600cb09b5210cfaf93a2fa66200472cd939a6
lyndon-thinkdwm <lyndon@directworksmedia.com>
2019-08-21 04:44:21 +0800

    Coverting to class component

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,18 +1,20 @@
-import React from "react";
-
+import React, { Component } from "react";
 import "./Person.css";
 
-const Person = props => {
-  console.log("[Person.js] rendering...");
-  return (
-    <div className="Person">
-      <p onClick={props.click}>
-        I'm {props.name} and I am {props.age} years old!
-      </p>
-      <p>{props.children}</p>
-      <input type="text" onChange={props.changed} value={props.name} />
-    </div>
-  );
-};
+class Person extends Component {
+  render() {
+    console.log("[Person.js] rendering...");
+    const { click, name, age, children, changed } = this.props;
+    return (
+      <div className="Person">
+        <p onClick={click}>
+          I'm {name} and I am {age} years old!
+        </p>
+        <p>{children}</p>
+        <input type="text" onChange={changed} value={name} />
+      </div>
+    );
+  }
+}
 
 export default Person;
